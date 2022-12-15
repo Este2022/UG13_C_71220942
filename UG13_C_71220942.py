546ce6c97407d20e4043b6a70c02bb48a90429a9
Este2022 <117894859+Este2022@users.noreply.github.com>
2022-12-15 16:26:54 +0700

    Add files via upload

---
+++ UG13_C_71220942.py
@@ -0,0 +1,62 @@
+import turtle
+s = turtle.Screen()
+t = turtle.Turtle()
+
+s.bgcolor('blue')
+
+#huruf E
+t.penup()
+t.goto(-100,45)
+t.pendown()
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(40)
+t.left(90)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(40)
+
+#angka 4
+
+t.penup()
+t.goto(20,45)
+t.pendown()
+t.left(90)
+t.forward(100)
+t.left(135)
+t.forward(40)
+t.left(135)
+t.forward(40)
+
+#huruf M
+t.penup()
+t.goto(100,45)
+t.pendown()
+t.left(90)
+t.forward(100)
+t.right(135)
+t.forward(20)
+t.left(90)
+t.forward(20)
+t.right(135)
+t.forward(100)
+
+
+
+
+
+
+
+
+
